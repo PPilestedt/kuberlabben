cca01aa8c1229e7b58f898f97f4d4264bdbe1dc0
Peter <peter.pilestedt@gmail.com>
2021-09-16 13:17:14 +0200

    Initial commit

---
+++ src/main/java/kuber/kuberlabben/KuberlabbenApplication.java
@@ -0,0 +1,13 @@
+package kuber.kuberlabben;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuberlabbenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuberlabbenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kuber/kuberlabben/KuberlabbenApplicationTests.java
@@ -0,0 +1,13 @@
+package kuber.kuberlabben;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuberlabbenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
